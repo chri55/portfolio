3abb5de98b048bf1e015f4d1bda5b6fff25d3769
Chris Till <cpaultill@gmail.com>
2020-06-22 15:23:25 -0400

    change aboutpage descriptions.

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -47,12 +47,12 @@ class AboutPage extends Component {
             <br></br>
             <h3>Experience</h3>
             <h4 className="exp"><strong>Researcher (DiSC)</strong></h4>
-            <p className="exp light"><em>January 2019 - Present</em></p>
+            <p className="exp light"><em>January 2019 - May 2020</em></p>
             <p className="exp">Worked for the Rutgers Camden Digital Studies Center (DiSC) researching harassment of online personalities. Specifically, we researched an "AI influencer" personality on Instagram that claimed to be a robot. In reality, a company was behind this character and that character was simply a 3D model set into the scenarios in her pictures. As a result, this character got a lot of harassment on Instagram, and our job was to collect and analyze the comments to determine how online harassment shown to these virtual entities was different to that shown to humans. More info can be found on <a href="http://club-405.com">Club 405's website.</a></p>
             <p className="exp"></p>
             <br></br>
             <h4 className="exp"><strong>Researcher (CCIB)</strong></h4>
-            <p className="exp light"><em>June 2018 - Present</em></p>
+            <p className="exp light"><em>June 2018 - June 2020</em></p>
             <p className="exp">Worked for the Rutgers Camden Center for Computational and Integrative Biology (CCIB) researching distributed search-and-evacuate algorithms using mobile agents. Our aim was to fully understand and recreate these algorithms in order to create the first visualizer to help other researchers in this field. These algorithms typically involve multiple mobile agents, (or for the sake of simplicity, bots) which are searching in an enclosed space for some sort of goal. As this is typically an evacuation problem, we say that the goal is an exit, and the algorithm terminates when a certain number of bots cooperate to find that exit. We created the initial framework for the simulator using JavaScript, and extended it to include the visualizer, which is made using <a href="https://d3js.org">D3.js.</a> We have since looked at other problems such as particle simulations on a triangular grid, and hope to create another simulation/visualization framework for this genre of problems. More information can be found at the project's website: <a href="#link-to-proj">Search and evacuations algorithms</a></p>
             <br></br>
             <h3>Education</h3>
@@ -63,11 +63,10 @@ class AboutPage extends Component {
             <h3>Awards & Honors</h3>
             <ul>
               <li>RUCHacks Winner</li>
-              <li>PHISIM award winner</li>
+              <li>PHISIM scholarship award winner</li>
               <li>Presented research at <a href="http://ccscne.org/">CCSCNE</a> for Rutgers Camden CCIB</li>
               <li>Presented research at <a href="https://www.acjs.org/default.aspx">ACJS</a> in San Antonio, and <a href="https://www.asc41.com/annualmeeting.html">ASC</a> in San Francisco, CA for Rutgers Camden DiSC</li>
             </ul>
-
           </div>
         </div>
       </MainLayout>
