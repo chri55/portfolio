55967d41a834cfdbdd0718ce5314f8f13e2a3999
Chris Till <cpaultill@gmail.com>
2022-06-14 18:54:27 -0400

    remove bit dependency

--- src/components/BottomNavigation/BottomNavigation.jsx
+++ src/components/BottomNavigation/BottomNavigation.jsx
@@ -1,41 +1,50 @@
-import React, { Component } from 'react';
-import { Link } from 'gatsby';
-import config from '../../../data/SiteConfig';
-import './BottomNavigation.css';
-import HomeIcon from '@bit/mui-org.material-ui-icons.home-rounded';
-import AboutIcon from '@bit/mui-org.material-ui-icons.perm-identity-sharp';
-import PortfolioIcon from '@bit/mui-org.material-ui-icons.photo';
-import BlogIcon from '@bit/mui-org.material-ui-icons.book-rounded';
-
+import React, { Component } from "react";
+import { Link } from "gatsby";
+import config from "../../../data/SiteConfig";
+import "./BottomNavigation.css";
+import { MdHome as HomeIcon } from "@react-icons/all-files/md/MdHome";
+import { MdPhoto as PortfolioIcon } from "@react-icons/all-files/md/MdPhoto";
+import { MdBook as BlogIcon } from "@react-icons/all-files/md/MdBook";
+import { RiUser3Fill as AboutIcon } from "@react-icons/all-files/ri/RiUser3Fill";
 
 class BottomNavigation extends Component {
   render() {
-    const isPartiallyActive = ({isPartiallyCurrent}) => {
-      return isPartiallyCurrent
-      ? { className: "active" }
-      : {}
-    }
+    const isPartiallyActive = ({ isPartiallyCurrent }) => {
+      return isPartiallyCurrent ? { className: "active" } : {};
+    };
     return (
       <div className="bottom-nav-root">
-        <Link to='/' className='nav-link' activeClassName="active">
+        <Link to="/" className="nav-link" activeClassName="active">
           <div className="nav-item">
             <HomeIcon></HomeIcon>
             <p>Home</p>
           </div>
         </Link>
-        <Link to='/about' className='nav-link' activeClassName="active">
+        <Link to="/about" className="nav-link" activeClassName="active">
           <div className="nav-item">
             <AboutIcon></AboutIcon>
             <p>Resume</p>
           </div>
         </Link>
-        <Link to='/portfolio' className='nav-link' activeClassName="active" getProps={isPartiallyActive}{...this.props}>
+        <Link
+          to="/portfolio"
+          className="nav-link"
+          activeClassName="active"
+          getProps={isPartiallyActive}
+          {...this.props}
+        >
           <div className="nav-item">
             <PortfolioIcon></PortfolioIcon>
             <p>Portfolio</p>
           </div>
         </Link>
-        <Link to='/blog/' className='nav-link' activeClassName="active" getProps={isPartiallyActive}{...this.props}>
+        <Link
+          to="/blog/"
+          className="nav-link"
+          activeClassName="active"
+          getProps={isPartiallyActive}
+          {...this.props}
+        >
           <div className="nav-item">
             <BlogIcon></BlogIcon>
             <p>Blog</p>
@@ -43,7 +52,7 @@ class BottomNavigation extends Component {
         </Link>
       </div>
     );
-  };
+  }
 }
 
 export default BottomNavigation;
--- src/templates/portfolio_item.jsx
+++ src/templates/portfolio_item.jsx
@@ -1,24 +1,20 @@
 import React, { Component } from "react";
 import Helmet from "react-helmet";
-import { Link } from 'gatsby';
+import { Link } from "gatsby";
 import MainLayout from "../layout/MainLayout";
 import About from "../components/About/About";
 import config from "../../data/SiteConfig";
-import BackIcon from "@bit/mui-org.material-ui-icons.navigate-before-sharp";
+
 import "../pages/index.css";
 import "./portfolio.css";
 import "./portfolio_item.css";
 
 function addTags(tags) {
   console.log(tags);
-  return (tags.map((elem) => {
+  return tags.map((elem) => {
     console.log(elem);
-    return(
-      <li key={`key-${elem.split()[0]}`}>
-        {elem}
-      </li>
-    );
-  }));
+    return <li key={`key-${elem.split()[0]}`}>{elem}</li>;
+  });
 }
 
 class PortfolioPage extends Component {
@@ -27,23 +23,32 @@ class PortfolioPage extends Component {
     return (
       <MainLayout>
         <div className="portfolio-container">
-          <Link to="/portfolio"><h3>Return to Portfolio</h3></Link>
-          <Helmet title={ `${config.siteTitle}` } />
+          <Link to="/portfolio">
+            <h3>Return to Portfolio</h3>
+          </Link>
+          <Helmet title={`${config.siteTitle}`} />
           <h2>{this.props.pageContext.portfolioItem.title}</h2>
           <p>{this.props.pageContext.portfolioItem.synopsis}</p>
           <section className="splash">
-            <img src={`${this.props.pageContext.portfolioItem.previewImageLink}`} alt={`Screenshot of ${this.props.pageContext.portfolioItem.title}`} className="featured"/>
+            <img
+              src={`${this.props.pageContext.portfolioItem.previewImageLink}`}
+              alt={`Screenshot of ${this.props.pageContext.portfolioItem.title}`}
+              className="featured"
+            />
           </section>
           <br></br>
-          <a class="no-underline"  href={`${this.props.pageContext.portfolioItem.link}`}><div class="project-link ">Link to Project</div></a>
+          <a
+            class="no-underline"
+            href={`${this.props.pageContext.portfolioItem.link}`}
+          >
+            <div class="project-link ">Link to Project</div>
+          </a>
           <h3>More about this project...</h3>
-          <hr className="divider"/>
+          <hr className="divider" />
           <p>{this.props.pageContext.portfolioItem.description}</p>
           <h3>Technologies used</h3>
-          <hr className="divider"/>
-          <ul>
-            {addTags(this.props.pageContext.portfolioItem.tags)}
-          </ul>
+          <hr className="divider" />
+          <ul>{addTags(this.props.pageContext.portfolioItem.tags)}</ul>
         </div>
       </MainLayout>
     );
